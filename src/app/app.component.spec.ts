396999155483054b46eac73bc54c058fef582675
panisin000 <45750570+panisin000@users.noreply.github.com>
2023-04-19 21:21:35 +0700

    homework

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('simple-profile-app app is running!');
+    // expect(compiled.querySelector('.content span')?.textContent).toContain('simple-profile-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PEAxODDS coming soon..');
   });
 });
